0b10fed17e8063a8fea552dbd46facbc91cc5093
zhaozhe <zhaozhe@xiaozhu.com>
2019-10-20 17:07:48 +0800

    update services

--- app/src/main/java/com/yu/zz/retrofitapt/weather/api/WeatherService.java
+++ app/src/main/java/com/yu/zz/retrofitapt/weather/api/WeatherService.java
@@ -4,8 +4,11 @@ import com.yu.zz.retrofitapt.weather.bean.WeatherBaseBean;
 import com.zz.yu.lib.ApiFactory;
 
 import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 @ApiFactory(createCode = "com.yu.zz.retrofitapt.weather.api.WeatherApi.Companion.getRetrofit().create(<ClassName>.class)")
 public interface WeatherService {
-    Observable<WeatherBaseBean> address(String prov, String needDay, String city, String area);
+    @GET("address")
+    Observable<WeatherBaseBean> address(@Query("area") String area, @Query("city") String city, @Query("needday") String needDay, @Query("prov") String prov);
 }
